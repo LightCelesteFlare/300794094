67e7b22362a2c537854f3361a0b43aad8aeb4aea
Brandon <Brandonkool16@gmail.com>
2018-03-06 17:16:32 -0500

    [Mod]
    Celestia

--- 300794094(Koo)ASS#3/Question1/Question1/Program.cs
+++ 300794094(Koo)ASS#3/Question1/Question1/Program.cs
@@ -9,86 +9,94 @@ namespace Question1
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            List<Inventory> items = new List<Inventory>();
-            items.Add(new Inventory() { ItemId = 83, item_Description = "Electric sander", Quantity = 7, unit_Price = 59.98 });
-            items.Add(new Inventory() { ItemId = 24, item_Description = "Power saw", Quantity = 18, unit_Price = 99.99 });
-            items.Add(new Inventory() { ItemId = 7, item_Description = "Sledge hammer", Quantity = 11, unit_Price = 21.55 });
-            items.Add(new Inventory() { ItemId = 77, item_Description = "Hammer", Quantity = 76, unit_Price = 11.99 });
-            items.Add(new Inventory() { ItemId = 39, item_Description = "Lawn mower", Quantity = 3, unit_Price = 79.95 });
-            items.Add(new Inventory() { ItemId = 68, item_Description = "Screwdriver", Quantity = 106, unit_Price = 7.99 });
-            items.Add(new Inventory() { ItemId = 56, item_Description = "Jig Saw", Quantity = 21, unit_Price = 11.95 });
-            items.Add(new Inventory() { ItemId = 3, item_Description = "Wrench", Quantity = 34, unit_Price = 7.95 });
-
+            var items = new[] {
+            new Inventory( 83, "Electric sander", 7, 59.98 ),
+            new Inventory(24,"Power saw", 18, 99.99 ),
+            new Inventory(7, "Sledge hammer", 11, 21.55 ),
+            new Inventory(77,"Hammer", 76, 11.99 ),
+            new Inventory(39,"Lawn mower", 3, 79.95 ),
+            new Inventory(68,"Screwdriver", 106, 7.99 ),
+            new Inventory(56,"Jig Saw", 21, 11.95 ),
+            new Inventory(3, "Wrench", 34, 7.95 )
+        }; // item list
+            Console.WriteLine("| ItemId | item_Description | Quantity | unit_Price |");
+            Console.WriteLine("|--------|------------------|----------|------------|");
             foreach (var n in items)
             {
                 Console.WriteLine(n);
+                Console.WriteLine("|--------|------------------|----------|------------|");
+            } // end foreach
+            // Question 1
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("1. sort the data by item description\n");
+            var ItemDescriptionSort = from b in items
+                                      orderby b.item_Description
+                                      select b;
+
+            foreach (var n in ItemDescriptionSort)
+            {
+                Console.WriteLine(n);
+            }
+            // Question 2
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("2. select the item description and quantity, and sort the reselts by quantity \n");
+            var Quantityitem = from b in items
+                               orderby b.Quantity
+                               select new { b.item_Description, b.Quantity };
+
+            Console.WriteLine("| item_Description | Quantity |");
+            Console.WriteLine("|------------------|----------|");
+            foreach (var n in Quantityitem)
+            {
+                Console.WriteLine("| {0,-16} | {1,-8} |", n.item_Description, n.Quantity);
             }
 
-            Console.WriteLine("Sort | MostExp | ReducePrice");
-                Console.Write("Enter a Command: ");
-                var com = Console.ReadLine();
+            // Question 3
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("3. select the item description and its total value\n");
+            var totalPrice = (from b in items
+                              select new { b.ItemId, b.item_Description, totalPrice = (b.Quantity * b.unit_Price) }).OrderByDescending(i => i.totalPrice);
 
-                if ("Sort" == com)
-                {
-                    Console.WriteLine("Items | ItemDescription | Quantity | UnitPrice");
-                    Console.Write("Enter the column name (no spaces): ");
-                    var comm = Console.ReadLine();
-
-                    // items#
-                    if ("Items" == comm)
-                    {
-                        var query = from b in items
-                                    orderby b.ItemId
-                                    select b;
-                    }
-                        
-                    if("ItemDescription" == comm)
-                    {
-                        var query = from b in items
-                                    orderby b.item_Description
-                                    select b;
-                    }
-
-                    if ("Quantity" == comm)
-                    {
-                        var query = from b in items
-                                    orderby b.Quantity
-                                    select b;
-                    }
-
-                    if ("UnitPrice" == comm)
-                    {
-                        var query = from b in items
-                                    orderby b.unit_Price
-                                    select b;
-                    }
-                                      
-                }
-                else if ("MostExp" == com)
-                {
-
-                double maxPrice = items.Max(t => t.unit_Price);
-                Console.WriteLine("{0} Highest price: {1} ",items ,maxPrice);
-                }
-
-                else if ("ReducePrice" == com)
-                {
-                foreach (var n in items)
-                {
-                    Console.WriteLine(n.unit_Price /10);
-                }
+            Console.WriteLine("| ItemId | item_Description | TotalPrice |");
+            Console.WriteLine("|--------|------------------|------------|");
+            foreach (var n in totalPrice)
+            {
+                Console.WriteLine("| {0,-6} | {1,-16} | {2,-10 :C} |", n.ItemId, n.item_Description, n.totalPrice);
             }
-                else
-                {
-                    Console.WriteLine("Invalid command");
-                    Console.WriteLine("Exiting Program");                  
-                }
+
+            // Question 4
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("4. Retrieve and display the most expensive item\n");
+
+            var MostExpen = from e in items
+                            orderby e.unit_Price
+                            select e;
+
+            Console.WriteLine("| ItemId | item_Description | Quantity | unit_Price |");
+            Console.WriteLine("|--------|------------------|----------|------------|");
+            Console.WriteLine(MostExpen.Last());
+
+            // Question 5
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("5. reduce and Unit price of all Items by 10%\n");
+            var reduction = from b in items
+                            select new { b.ItemId, b.item_Description, b.Quantity, Reduces = (b.unit_Price - (b.unit_Price * .1)) };
+
+            Console.WriteLine("| ItemId | item_Description | Quantity | unit_Price |");
+            Console.WriteLine("|--------|------------------|----------|------------|");
+            foreach (var r in reduction)
+            {
+                Console.WriteLine("| {0,-6} | {1,-16} | {2,-7}  | {3,-10 :C} |", r.ItemId, r.item_Description, r.Quantity, r.Reduces);
+                Console.WriteLine("|--------|------------------|----------|------------|");
+            }
             Console.ReadKey();
         }
-            
+    }
+
+
         public class Inventory
         {
             // the Item#
@@ -101,12 +109,18 @@ namespace Question1
             // Unit Price
             public double unit_Price { get; set; }
 
-            public override string ToString()
+            public Inventory(int Itemid, string item_Desc, int Quantity, double unitPrice)
             {
-                return string.Join(" ", new string[] {
-                string.Format("| ItemId = {0} | item_Description = {1} | Quantity = {2} | unit_Price = {3} |", this.ItemId, this.item_Description, this.Quantity, this.unit_Price)});
+                this.ItemId = Itemid;
+                this.item_Description = item_Desc;
+                this.Quantity = Quantity;
+                this.unit_Price = unitPrice;
             }
+
+            public override string ToString() =>
+                $"{"| "}{ItemId,-7}{"| "}{item_Description,-17}{"| "}{Quantity,-6}{"| $",6}{unit_Price,-10}{"|"}";
+
         }       
     }
-}
+
 
--- 300794094(Koo)ASS#3/Question2/Program.cs
+++ 300794094(Koo)ASS#3/Question2/Program.cs
@@ -10,6 +10,123 @@ namespace Question2
     {
         static void Main(string[] args)
         {
+            var clients = new[]
+            {
+                new Client( 1 , "BK Associates", "Commercial", "Toronto", 230000, true),
+                new Client( 6 , "Bick", "Industrial", "Dallas", 679000, false),
+                new Client( 19 , "TPH", "Government", "Athlanta", 986000, true),
+                new Client( 12 , "Crow", "Industrial", "Phoenix", 126000, false),
+                new Client( 56 , "TX Electric", "Industrial", "Portland", 564000, true),
+                new Client( 42 , "GRB", "Government", "Omaha", 437000, false),
+                new Client( 98 , "LB&B", "Commercial", "Toronto", 990000, true),
+                new Client( 44 , "H&P", "Commercial", "Denver", 122000, true),
+            };
+
+            Console.WriteLine("| ClientID |   Client Name   | Client Services | Client City | Client Revenue | Client Type |");
+            Console.WriteLine("|----------|-----------------|-----------------|-------------|----------------|-------------|");
+            foreach (var n in clients)
+            {
+                Console.WriteLine(n);
+            } // end foreach
+
+            //Question 1
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------");
+            Console.WriteLine("1. sort the data by Client ID\n");
+            var ClientSort = from b in clients
+                             orderby b.ClientID
+                             select b;
+
+            foreach (var n in ClientSort)
+            {
+                Console.WriteLine(n);
+            }
+            // Question 2
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------");
+            Console.WriteLine("2. sort the data by Name and City \n");
+            var NameCitySort = from b in clients
+                               orderby b.ClientName, b.City
+                               select new { b.ClientID, b.ClientName, b.ClientServices, b.City, b.Revenue, b.action };
+
+            Console.WriteLine("| ClientID |   Client Name   | Client Services | Client City | Client Revenue | Client Type |");
+            Console.WriteLine("|----------|-----------------|-----------------|-------------|----------------|-------------|");
+            foreach (var n in NameCitySort)
+            {
+                Console.WriteLine("| {0,-8} | {1,-15} | {2,-15} | {3,-11} | {4,-14 :C} | {5,-11} |", n.ClientID, n.ClientName, n.ClientServices, n.City, n.Revenue, n.action);
+            }
+
+            // Question 3
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------");
+            Console.WriteLine("3. Select the Government clients and sort the results by Revenue\n");
+            var GovernmentRev = from b in clients
+                                where b.ClientServices == "Government"
+                                select b;
+            Console.WriteLine("| ClientID |   Client Name   | Client Services | Client City | Client Revenue | Client Type |");
+            Console.WriteLine("|----------|-----------------|-----------------|-------------|----------------|-------------|");
+            foreach (var n in GovernmentRev)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Question 4
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------");
+            Console.WriteLine("4. Select only the Active clients and calculate their total Revenue\n");
+
+            var ActiveSum = from b in clients
+                            where b.action == true
+                            group b by 1 into g                        
+                            select new { Total = g.Sum(x => x.Revenue) };
+
+            foreach (var m in ActiveSum)
+            {
+                Console.WriteLine("Total Revenue: {0:C}",m.Total);
+            } // end foreach
+
+            // Question 5
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------");
+            Console.WriteLine("5. retrieve and display the most important Client\n");
+            var VIP = from b in clients
+                      where b.action == true
+                      orderby b.Revenue descending
+                      select b;
+
+            Console.WriteLine("| ClientID |   Client Name   | Client Services | Client City | Client Revenue | Client Type |");
+            Console.WriteLine("|----------|-----------------|-----------------|-------------|----------------|-------------|");
+            foreach (var g in VIP)
+            {
+                Console.WriteLine(g);
+            } // end foreach
+
+            Console.ReadKey();
         }
     }
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string ClientName { get; set; }
+        public string ClientServices { get; set; }
+        public string City { get; set; }
+        public double Revenue { get; set; }
+        public bool action { get; set; }
+    
+        public Client(int ClientID, string ClientName, string Service, string City, double Revenue, bool action)
+        {
+            this.ClientID = ClientID;
+            this.ClientName = ClientName;
+            this.ClientServices = Service;
+            this.City = City;
+            this.Revenue = Revenue;
+            if (action == true) { 
+                this.action = true;
+            }
+            else
+            {
+                this.action = false;
+            }
+        }
+
+        public override string ToString() =>
+             $"{"| "}{ClientID,-9}{"| "}{ClientName,-16}{"| "}{ClientServices,-12}{"| ",6}{City,-12}{"| $"}{Revenue,-14}{"| "}{action,-12}{"|"}";
+              
+    }
+
 }
