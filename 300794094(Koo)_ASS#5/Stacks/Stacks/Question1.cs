ffc3f48d400f402755682cc871ba24afaf6ed94b
Brandon <BrandonKool16@gmail.com>
2018-04-07 20:45:20 -0400

    [Add]

--- 300794094(Koo)_ASS#5/Stacks/Stacks/Question1.cs
+++ 300794094(Koo)_ASS#5/Stacks/Stacks/Question1.cs
@@ -12,17 +12,38 @@ namespace Stacks
         /* Write a program that inputs a line of text and uses a stack object
            to display the line reversed
         */
-        public static void Main() { 
-            
+        public static void Main() {
+            string text = "First one in, Second one in, Third one in, Fourth one in";
+            Console.WriteLine("Raw Text is shown");
+            Console.Write(text);
+
+            Console.WriteLine("\n\n---Separating the text---");
             Stack myStacks = new Stack();
-            myStacks.Push("First one in");
-            myStacks.Push("Second one in");
-            myStacks.Push("Third one in");
-            myStacks.Push("Fourth one in");
+            for(int i = 0; i < 4; i++)
+            {
+                string[] partline = text.Split(',');
+                myStacks.Push(partline[i]);
+            }
+
+            //myStacks.Push("First one in");
+            //myStacks.Push("Second one in");
+            //myStacks.Push("Third one in");
+            //myStacks.Push("Fourth one in");
 
             // Display the Stacks
             Console.Write("Stack Values:");
             PrintValues( myStacks, '\t');
+            Console.WriteLine("Hit any key to continue");
+            Console.ReadKey();
+
+            Console.WriteLine("\nStack Values to Array");
+            Console.Write("Stack Values:");
+            PrintValues(myStacks, '\t');
+
+            Console.WriteLine("\n--then reverse it--");
+            Console.Write("Stack Values:");
+            PrintValues(myStacks.ToArray().Reverse());
+
             Console.ReadKey();
 
 
@@ -31,7 +52,13 @@ namespace Stacks
         public static void PrintValues(IEnumerable myCollection, char mySeparator)
         {
             foreach (Object obj in myCollection)
-                Console.Write("{0}{1} |", mySeparator, obj);
+                Console.Write("{0}{1} ", mySeparator, obj);
+            Console.WriteLine();
+        }
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+                Console.Write("    {0}", obj);
             Console.WriteLine();
         }
     }
